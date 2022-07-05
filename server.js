1ffd50fb3c1e27c4a8c78924bbd282ab3bbd465b
TK54 <takegoshi.1006@i.softbank.jp>
2022-07-05 11:14:31 +0900

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ serve(async (req) => {
     const requestJson = await req.json();
     const nextWord = requestJson.nextWord;
     if (nextWord.length > 0 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)) {
-        return new Response("前の単語に続いていません。", { status: 4000 });
+        return new Response("前の単語に続いていません。", { status: 400 });
       }
       
     previousWord = nextWord;
