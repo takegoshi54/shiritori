03ce504e1ef983a350f01abc31c59405acedabb8
TK54 <takegoshi.1006@i.softbank.jp>
2022-07-05 12:54:02 +0900

    update

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ serve(async (req) => {
     }
     if (nextWord.length > 0 &&  "ん" == nextWord.charAt(nextWord.length - 1)) { //「ん」を検出
       return new Response("「ん」で終わったのであなたの負けです", { status: 400 }); //TODO ゲーム終了
-      return 0;
+      
     }
     if (nextWord.length > 0 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)) {
         return new Response("前の単語に続いていません。", { status: 400 });
